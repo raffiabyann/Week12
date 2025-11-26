c8f0bc5a2c61ec92baa5e1a2546218e4b5fde495
raffiabyann <raffizizou4@gmail.com>
2025-11-26 10:56:42 +0700

    Read myfile.txt

--- src/week12/raffi/id/umn/ac/file_handling.java
+++ src/week12/raffi/id/umn/ac/file_handling.java
@@ -1,21 +1,25 @@
 package week12.raffi.id.umn.ac;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class file_handling {
-	 public static void main(String[] args) {
 
-	        try {
-	            File Obj = new File("myfile.txt");
-	            if (Obj.createNewFile()) {
-	                System.out.println("File created: " + Obj.getName());
-	            } else {
-	                System.out.println("File already exists.");
-	            }
-	        } catch (IOException e) {
-	            System.out.println("An error has occurred.");
-	            e.printStackTrace();
-	        }
+    public static void main(String[] args) {
 
-	    }
+        try {
+            File Obj = new File("myfile.txt");
+            Scanner Reader = new Scanner(Obj);
+            while (Reader.hasNextLine()) {
+                String data = Reader.nextLine();
+                System.out.println(data);
+            }
+            Reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+
+    }
 }
+
