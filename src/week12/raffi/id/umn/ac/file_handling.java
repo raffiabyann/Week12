df4297df2644a639f7ec156fe54d11ebd3c30122
raffiabyann <raffizizou4@gmail.com>
2025-11-26 10:51:15 +0700

    Write File myfile.txt

---
+++ src/week12/raffi/id/umn/ac/file_handling.java
@@ -0,0 +1,21 @@
+package week12.raffi.id.umn.ac;
+import java.io.File;
+import java.io.IOException;
+
+public class file_handling {
+	 public static void main(String[] args) {
+
+	        try {
+	            File Obj = new File("myfile.txt");
+	            if (Obj.createNewFile()) {
+	                System.out.println("File created: " + Obj.getName());
+	            } else {
+	                System.out.println("File already exists.");
+	            }
+	        } catch (IOException e) {
+	            System.out.println("An error has occurred.");
+	            e.printStackTrace();
+	        }
+
+	    }
+}
