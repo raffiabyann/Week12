6bcab29dab658fe3b73c0544b8f6eba32c3cbd2d
raffiabyann <raffizizou4@gmail.com>
2025-11-26 11:02:37 +0700

    Store object to person.ser

---
+++ src/week12/raffi/id/umn/ac/Person.java
@@ -0,0 +1,47 @@
+package week12.raffi.id.umn.ac;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private int age;
+    private String campus;
+
+    public Person(String name, int age, String campus) {
+        this.setName(name);
+        this.setAge(age);
+        this.setCampus(campus);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + getName() + "', age=" + getAge() +
+               ", campus='" + getCampus() + "'}";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+}
+
